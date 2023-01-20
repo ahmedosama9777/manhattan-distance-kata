4b57cf129124a758017905cc26cbc0ce01f49f63
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-20 11:00:41 +0200

    Add manhattan distance

--- manhattan_distance.py
+++ manhattan_distance.py
@@ -1,7 +1,14 @@
 class Point:
+    
     def __init__(self, x: int, y: int):
-        super(Point, self).__setattr__("__x", x)
-        super(Point, self).__setattr__("__y", y)
+        super(Point, self).__setattr__("_x", x)
+        super(Point, self).__setattr__("_y", y)
 
     def __setattr__(self, name, value):
         raise AttributeError
+    
+    def distanceFrom(self, point_b):
+        return abs(self._x - point_b._x) + abs(self._y - point_b._y)
+
+def manhattanDistance(point_a: Point, point_b: Point) -> int:
+    return point_a.distanceFrom(point_b)
--- manhattan_distance_test.py
+++ manhattan_distance_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from manhattan_distance import Point
+from manhattan_distance import Point, manhattanDistance
 
 
 class TestPoint(TestCase):
@@ -8,9 +8,16 @@ class TestPoint(TestCase):
         self.point = Point(1, 0)
 
     def test_creation_of_point(self):
-        with self.assertRaises(AttributeError):
-            x = self.point.x
+        x = self.point._x
 
     def test_point_is_immutable(self):
         with self.assertRaises(AttributeError):
-            self.point.x = 2
+            self.point._x = 2
+
+    def test_get_manhattan_distance(self):
+        point_a = Point(3, 5)
+        point_b = Point(1, 2)
+
+        manhattan_distance = manhattanDistance(point_a, point_b)
+
+        self.assertEqual(manhattan_distance, 5)
